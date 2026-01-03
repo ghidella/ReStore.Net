92745769f47f202b270e66caf672e7d88580ef69
ghidella <raphaelfghidella@gmail.com>
2026-01-03 20:06:14 -0300

    add basic react front app and connect to API

---
+++ client/src/App.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+
+function App() {
+  const [products, setProducts] = useState<{ id: number; name: string; price: number }[]>([]);
+
+
+useEffect(() => {
+  fetch('https://localhost:5001/api/products')
+    .then(response => response.json())
+    .then(data => setProducts(data))
+    .catch(error => console.error('Error fetching products:', error));
+}, []);
+
+const addProduct = () => {
+  setProducts(prevState => [...prevState, { id: prevState.length + 1, name: 'Product ' + (prevState.length + 1), price: (prevState.length + 1) * 10.0 }]);
+}
+
+  return (
+    <div>
+      <h1>ReStore.Net</h1>
+      <ul>
+        {products.map(items => (
+          <li key={items.id}>{items.name} - {items.price}</li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add Product</button>
+    </div>
+  )
+}
+
+export default App
