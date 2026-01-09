bb6a5643900231aee914f48eff700ccf8a469659
ghidella <raphaelfghidella@gmail.com>
2026-01-09 08:47:06 -0300

    add ts type product

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react"
+import type { Product } from "./product";
 
 function App() {
-  const [products, setProducts] = useState<{ id: number; name: string; price: number }[]>([]);
+    const [products, setProducts] = useState<Product[]>([]);
 
 
-useEffect(() => {
+useEffect(() => {    
   fetch('https://localhost:5001/api/products')
     .then(response => response.json())
     .then(data => setProducts(data))
@@ -12,7 +13,17 @@ useEffect(() => {
 }, []);
 
 const addProduct = () => {
-  setProducts(prevState => [...prevState, { id: prevState.length + 1, name: 'Product ' + (prevState.length + 1), price: (prevState.length + 1) * 10.0 }]);
+  setProducts(prevState => [...prevState, 
+    { 
+      id: prevState.length + 1,
+      name: 'Product ' + (prevState.length + 1),
+      description: 'Description for Product ' + (prevState.length + 1),
+      price: (prevState.length + 1) * 10.0,
+      pictureUrl: 'https://via.placeholder.com/150',
+      type: 'Type ' + (prevState.length + 1),
+      brand: 'Brand ' + (prevState.length + 1),
+      quantityInStock: 100
+    }]);
 }
 
   return (
