ea8c1bb1b2ca5899e0ea28fea523221ab259b71d
ghidella <raphaelfghidella@gmail.com>
2026-01-13 08:59:46 -0300

    add navBar material ui

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import type { Product } from "../models/product";
 import Catalog from "../features/catalog/Catalog";
-import { Box, Button, Container, Typography } from "@mui/material";
+import { Container } from "@mui/material";
+import NavBar from "./NavBar";
 
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
@@ -13,32 +14,13 @@ function App() {
       .catch((error) => console.error("Error fetching products:", error));
   }, []);
 
-  const addProduct = () => {
-    setProducts((prevState) => [
-      ...prevState,
-      {
-        id: prevState.length + 1,
-        name: "Product " + (prevState.length + 1),
-        description: "Description for Product " + (prevState.length + 1),
-        price: (prevState.length + 1) * 10.0,
-        pictureUrl: "https://via.placeholder.com/150",
-        type: "Type " + (prevState.length + 1),
-        brand: "Brand " + (prevState.length + 1),
-        quantityInStock: 100,
-      },
-    ]);
-  };
-
   return (
-    <Container maxWidth="xl">
-      <Box display='flex' justifyContent='center' gap={3} marginY={3}>
-        <Typography variant="h4">ReStore.Net</Typography>
-        <Button variant="contained" color="primary" onClick={addProduct}>
-          Add Product
-        </Button>
-      </Box>
-      <Catalog products={products} />
-    </Container>
+    <>
+      <NavBar />
+      <Container maxWidth="xl" sx={{ mt: 14 }}>
+        <Catalog products={products} />
+      </Container>
+    </>
   );
 }
 
