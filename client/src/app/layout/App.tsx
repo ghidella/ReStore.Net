05154e1373a58e130979a8c945c20a4e6bc43910
ghidella <raphaelfghidella@gmail.com>
2026-01-13 22:41:52 -0300

    add routing in react and syle for navBar

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,6 +1,4 @@
-import { useEffect, useState } from "react";
-import type { Product } from "../models/product";
-import Catalog from "../features/catalog/Catalog";
+import { useState } from "react";
 import {
   Box,
   Container,
@@ -9,9 +7,9 @@ import {
   ThemeProvider,
 } from "@mui/material";
 import NavBar from "./NavBar";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const [products, setProducts] = useState<Product[]>([]);
   const [darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? "dark" : "light";
   const theme = createTheme({
@@ -25,14 +23,7 @@ function App() {
 
   const toggleDarkMode = () => {
     setDarkMode(!darkMode);
-  }
-
-  useEffect(() => {
-    fetch("https://localhost:5001/api/products")
-      .then((response) => response.json())
-      .then((data) => setProducts(data))
-      .catch((error) => console.error("Error fetching products:", error));
-  }, []);
+  };
 
   return (
     <ThemeProvider theme={theme}>
@@ -48,7 +39,7 @@ function App() {
         }}
       >
         <Container maxWidth="xl" sx={{ mt: 8 }}>
-          <Catalog products={products} />
+          <Outlet />
         </Container>
       </Box>
     </ThemeProvider>
